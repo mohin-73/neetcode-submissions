798785221aa0a1db67bb0f32592227cfeb00e1ee
Eklas Mohin <mohin73k@gmail.com>
2026-07-30 06:12:09 +0600

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minCost(vector<int>& cost, vector<int>& dp, int n) {
+        if (n <= 1) {
+            return dp[n] = 0;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int oneJump = minCost(cost, dp, n - 1) + cost[n - 1];
+        int twoJump = minCost(cost, dp, n - 2) + cost[n - 2];
+        return dp[n] = min(oneJump, twoJump);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = (int)cost.size();
+        vector<int> dp(n + 1, -1);
+        return minCost(cost, dp, n);
+    }
+};

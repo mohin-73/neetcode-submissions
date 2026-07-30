d15141a156c4897991c8d513bdc3fcf79e1df35f
Eklas Mohin <mohin73k@gmail.com>
2026-07-31 05:38:56 +0600

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+        unordered_map<char, int> freq;
+        for (char ch : s) {
+            ++freq[ch];
+        }
+        for (char ch : t) {
+            if (--freq[ch] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};

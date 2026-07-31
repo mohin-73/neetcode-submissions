344da5f90a2613a725522beab6b2321bad240115
Eklas Mohin <mohin73k@gmail.com>
2026-07-31 14:57:34 +0600

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mn = prices[0], profit = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            profit = max(profit, prices[i] - mn);
+            mn = min(mn, prices[i]);
+        }
+        return profit;
+    }
+};

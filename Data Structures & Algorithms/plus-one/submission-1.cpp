f79ec92768e4f34861c5dbf918ba67b36d09da10
Eklas Mohin <mohin73k@gmail.com>
2026-08-03 14:50:14 +0600

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = (int)digits.size() - 1;
+        for (int i = n; i >= 0; --i) {
+            if (digits[i] < 9) {
+                ++digits[i];
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits[0] = 1;
+        digits.push_back(0);
+        return digits;
+    }
+};

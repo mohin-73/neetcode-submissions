257994d3a520a1638a749d44933becacb79fee9e
Eklas Mohin <mohin73k@gmail.com>
2026-08-05 18:12:01 +0600

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s.empty()) {
+            return true;
+        }
+        int curr = 0, len = ssize(s);
+        for (char ch : t) {
+            if (s[curr] == ch) {
+                ++curr;
+            }
+            if (curr == len) {
+                return true;
+            }
+        }
+        return false;
+    }
+};

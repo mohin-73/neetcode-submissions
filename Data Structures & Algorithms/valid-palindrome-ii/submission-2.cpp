a16f603c08539815418f34f61f6a462126b331f8
Eklas Mohin <mohin73k@gmail.com>
2026-08-06 05:42:49 +0600

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string &s, int left, int right) {
+        while (left < right) {
+            if (s[left++] != s[right--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int left = 0, right = ssize(s) - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            } else {
+                ++left;
+                --right;
+            }
+        }
+        return true;
+    }
+};

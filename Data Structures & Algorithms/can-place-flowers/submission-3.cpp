22c75696958e554f97bf6b25f2b63af373f04788
Eklas Mohin <mohin73k@gmail.com>
2026-08-09 19:34:06 +0600

    Add: can-place-flowers - submission-3

---
+++ Data Structures & Algorithms/can-place-flowers/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& nums, int n) {
+        int k = ssize(nums);
+        nums.insert(nums.begin(), 0);
+        nums.push_back(0);
+        int cnt = 0;
+        for (int i = 1; i <= k; ++i) {
+            if (nums[i] == 0 && nums[i - 1] == 0 && nums[i + 1] == 0) {
+                ++cnt;
+                if (cnt == n) {
+                    return true;
+                }
+                nums[i] = 1;
+            }
+        }
+        return cnt >= n;
+    }
+};

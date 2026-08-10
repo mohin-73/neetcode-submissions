261d466765700624f3cdc00f51af544ea2f8eabc
Eklas Mohin <mohin73k@gmail.com>
2026-08-10 20:29:18 +0600

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-3

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxDifference(string s) {
+        vector<int> freq(26, 0);
+        int odd = INT_MIN, even = INT_MAX;
+        for (char ch : s) {
+            ++freq[ch - 'a'];
+        }
+        for (int i = 0; i < 26; ++i) {
+            if (!(freq[i] % 2) && freq[i] > 0) {
+                even = min(even, freq[i]);
+            } else if (freq[i] % 2) {
+                odd = max(odd, freq[i]);
+            }
+        }
+        return odd - even;
+    }
+};

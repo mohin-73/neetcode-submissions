90c76f8de1ef10baffd4f9637cccc4d0a7e817ee
Eklas Mohin <mohin73k@gmail.com>
2026-08-26 01:52:06 +0600

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x <= 1) {
+            return x;
+        }
+        int low = 1, high = x / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid == x / mid) {
+                if (mid * mid == x) {
+                    return mid;
+                }
+                low = mid + 1;
+            } else if (mid > x / mid) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
+    }
+};
